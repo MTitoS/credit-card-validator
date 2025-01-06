5b31f57a3285205c1bd65505ee0e3ba9ad05195f
Matheus Tito <mthsilva0@gmail.com>
2025-01-06 14:26:29 -0300

    Fixed problem with for loop and added commentary

--- credit.c
+++ credit.c
@@ -1,25 +1,35 @@
-#include <stdio.h>;
-#include <string.h>;
-#include <math.h>;
+#include <stdio.h>
+#include <string.h>
+
+int reverseCardNumber (char cardNumber[30]);
 
 void main (void) {
-
-    int cardNumber;
+    char cardNumber[30];
 
     do {
-        printf("Type your card number:");
-        scanf("%d",&cardNumber);
+        printf("Type your card number: ");
+        scanf("%s", cardNumber);
         while (getchar() != '\n');
 
-    } while (cardNumber < 0);
+    } while (cardNumber[0] == '-');
 
-    validateCard(cardNumber);
+    reverseCardNumber(cardNumber);
 }
 
-void validateCard (cardNumber) {
-    int cardNumberReversed;
+int reverseCardNumber (char cardNumber[30]) {
+    char cardNumberReversed[30];
+    int stringLength = strlen(cardNumber);
+    int j = 0;
 
-    for (int i = 0; i < cardNumber; i++) {
-        cardNumberReversed;
+    // I tem que ser definido como stringLenght - 1 porque a função strlen() descarta o último char que no caso é o caractere nulo que existe no final das strings ("\0")
+    // Se não for definido como stringLength -1, então ele irá tentar buscar um caractere inexistente, e no final não printará nada por conta desse comportamento.
+    for (int i = stringLength - 1; i >= 0; i--, j++) {
+        cardNumberReversed[j] = cardNumber[i];
     }
+    
+    cardNumberReversed[j] = '\0';
+
+    printf("Credit card number reversed: %s\n", cardNumberReversed);
+
+    return 0;
 }
